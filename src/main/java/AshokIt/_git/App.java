435fd53ec3bf916afe80e7c811b2f8c4411c94f0
Jrtp-430 <ramaraogartam430@gmail.com>
2021-11-18 16:46:55 -0700

    add y variable

--- src/main/java/AshokIt/_git/App.java
+++ src/main/java/AshokIt/_git/App.java
@@ -1,8 +1,10 @@
 package AshokIt._git;
 
 public class App {
+static int y=20;
   public static void main(String[] args) {
     System.out.println("Hello World!");
+     System.out.println(y); 
   }
 public void m2(){
 System.out.println("m2() method");
