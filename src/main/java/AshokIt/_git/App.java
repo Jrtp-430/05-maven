ce5ad2abc7b6c248a3e0daf0f3d41a1c1d5e35ba
Jrtp-430 <ramaraogartam430@gmail.com>
2021-11-18 16:41:09 -0700

    add x Variable

--- src/main/java/AshokIt/_git/App.java
+++ src/main/java/AshokIt/_git/App.java
@@ -1,8 +1,10 @@
 package AshokIt._git;
 
 public class App {
+static int x=10;
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println(x); 
   }
 public void m2(){
 System.out.println("m2() method");
