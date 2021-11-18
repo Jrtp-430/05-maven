4f97a4ca66fa8dc9d25bb2ff5c065d98428a2593
Jrtp-430 <ramaraogartam430@gmail.com>
2021-11-18 13:20:08 -0700

    add m1() method

--- src/main/java/AshokIt/_git/App.java
+++ src/main/java/AshokIt/_git/App.java
@@ -4,4 +4,7 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
-}
+  public void m1(){
+    System.out.println("Hello World!");
+  }
+}  
